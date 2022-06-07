6b316f9e3782fe03ef466ca2220acfd17136a259
mendoza099 <sergiomendozadominguez@gmail.com>
2022-06-07 21:53:02 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  console.log("hello rigo from the consolo!");
+};
+
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+  return who[whoIndx] + " " + what[whatIndx] + " " + when[whenIndx];
 };
